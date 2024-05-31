b623b93307e27dd5ec351cb07e2c6c7a34ac5f60
milovidov <milovidovgeorgy@yandex.ru>
2024-05-31 16:17:21 +0300

    fix: checkstyle

--- src/main/java/ru/practicum/shareit/errors/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/errors/ErrorHandler.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.shareit.exceptions.*;
 
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice
