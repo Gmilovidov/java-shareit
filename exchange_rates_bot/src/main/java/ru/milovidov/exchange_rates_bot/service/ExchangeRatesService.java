bbada8335eecc8da2df51f8674847b67dcee3acc
Gmilovidov <milovidovgeorgy@yandex.ru>
2024-08-15 01:49:36 +0300

    fix: remove bot

--- exchange_rates_bot/src/main/java/ru/milovidov/exchange_rates_bot/service/ExchangeRatesService.java
+++
@@ -1,9 +0,0 @@
-package ru.milovidov.exchange_rates_bot.service;
-
-import ru.milovidov.exchange_rates_bot.ServiceException;
-
-public interface ExchangeRatesService {
-    String getUSDExchangeRate() throws ServiceException;
-
-    String getEURExchangeRate() throws ServiceException;
-}
